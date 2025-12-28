b15ce24184b2f323787ac373f34aefa6706f5185
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2025-12-28 20:57:01 +0600

    basic setup complete with es6 - express js

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-import express from express;
-import cors from cors;
+import cors from "cors";
+import express from "express";
 
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send(" API is running");
+});
+
+// server
+app.listen(port, () => {
+  console.log(` Server running on http://localhost:${port}`);
+});
